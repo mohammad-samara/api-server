a2b0d10971ba1e1cd719721311586039ccfc02a5
mohammad-samara <mohammad.ab.samara@gmail.com>
2020-10-28 12:49:35 +0300

    add cart

--- middleware/params.js
+++ middleware/params.js
@@ -3,6 +3,7 @@
 const categoriesModel = require('../lib/models/categories-collection.js');
 const productsModel = require('../lib/models/products-collection.js');
 const todoModel = require('../lib/models/todo-collection.js');
+const cartModel = require('../lib/models/cart-collection');
 
 /**
  * (Middleware) will select the correct model for the requested route
@@ -30,6 +31,10 @@ function getModel(req, res, next) {
     return;
   case 'todo':
     req.model = todoModel;
+    next();
+    return;
+  case 'cart':
+    req.model = cartModel;
     next();
     return;
   default:
